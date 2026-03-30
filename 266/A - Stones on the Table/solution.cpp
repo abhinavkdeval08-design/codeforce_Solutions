3734058b90220c996780d55b9794561e5945d200
Abhinav Deval <abhinavkdeval08@gmail.com>
2026-03-31 00:26:45 +0530

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std;int main(){    string s;    int n;    int count = 0;    cin >> n >>  s;    for(int i=0;i<n; i++){    if(s[i] == s[i+1]){    count++;    }     }    cout << count;    return 0;}
