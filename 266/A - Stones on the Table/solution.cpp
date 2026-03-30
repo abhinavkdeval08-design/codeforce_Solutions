24dfc8b63e411dc1b8cecbb9ebf0697a1eafef21
Abhinav Deval <abhinavkdeval08@gmail.com>
2026-03-31 00:37:55 +0530

    Improve formatting and readability of solution.cpp
    Refactor code for better readability and formatting.

--- 266/A - Stones on the Table/solution.cpp
+++ 266/A - Stones on the Table/solution.cpp
@@ -1,1 +1,13 @@
-#include<bits/stdc++.h>using namespace std;int main(){    string s;    int n;    int count = 0;    cin >> n >>  s;    for(int i=0;i<n; i++){    if(s[i] == s[i+1]){    count++;    }     }    cout << count;    return 0;}
+#include<bits/stdc++.h>
+using namespace std;
+int main(){    
+  string s;    
+  int n;    
+  int count = 0;    
+  cin >> n >>  s;    
+  for(int i=0;i<n; i++){    
+    if(s[i] == s[i+1]){    
+      count++;    } }    
+  cout << count;    
+  return 0;
+}
