b2c0ad10fd8b3c856f30ce374f662c51c4ae2b61
Abhinav Deval <abhinavkdeval08@gmail.com>
2026-05-29 00:05:47 +0530

    Create 1635-NumberOfGoodPairs

---
+++ LeetCode/1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.cpp
@@ -0,0 +1,15 @@
+// Last updated: 29/05/2026, 00:05:45
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int count =0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
