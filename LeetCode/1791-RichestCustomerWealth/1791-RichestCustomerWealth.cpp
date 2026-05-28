d0961b7a69527b370b67f425057ba1079de7ea21
Abhinav Deval <abhinavkdeval08@gmail.com>
2026-05-29 00:05:37 +0530

    Create 1791-RichestCustomerWealth

---
+++ LeetCode/1791-RichestCustomerWealth/1791-RichestCustomerWealth.cpp
@@ -0,0 +1,13 @@
+// Last updated: 29/05/2026, 00:05:35
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth =0;
+        for(int i=0; i<accounts.size(); i++){
+            int sum = 0 ;
+            for(int j=0; j<accounts[i].size(); j++){
+            sum +=  accounts[i][j] ; 
+            maxWealth = max(maxWealth, sum); }
+        }
+    return maxWealth; }
+};
